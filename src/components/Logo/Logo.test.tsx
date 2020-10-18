5811bd737e6beb0416a487ddc32a80ed47c8d9e6
MisterCrude <andrzej.pluszcz@gmail.com>
2020-10-18 22:40:47 +0200

    init(styleguidis): setup

---
+++ src/components/Logo/Logo.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import { Logo } from './Logo';
+
+it('Should render Login component', () => {
+  render(<Logo />);
+
+  expect(screen().getByTestId('logo')).toBeInTheDocument();
+});
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Logo: React.FC = () => <div data-testid="login">Test</div>;
